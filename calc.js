932aab12baa3caab4ab340beaee23ce30b2cc846
s4nch3z-137 <70666788+s4nch3z-137@users.noreply.github.com>
2021-12-08 01:03:21 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,6 @@
+var i =0;
+var i = document.getElementsByClassName("pop")[0].value;
+function change() {
+var i = document.getElementsByClassName("pop")[0].value;
+document.getElementsByClassName("anjam")[0].innerHTML = (Math.log(i)/Math.log(2)) ;
+}
