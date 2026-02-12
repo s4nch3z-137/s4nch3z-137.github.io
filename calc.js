6faf856db33c6bfda69fa0733423002427cb9e77
s4nch3z-137 <70666788+s4nch3z-137@users.noreply.github.com>
2026-02-12 05:02:23 +0300

    Delete calc.js

--- calc.js
+++
@@ -1,6 +0,0 @@
-var i =0;
-var i = document.getElementsByClassName("pop")[0].value;
-function change() {
-var i = document.getElementsByClassName("pop")[0].value;
-document.getElementsByClassName("anjam")[0].innerHTML = (Math.log(i)/Math.log(2)) ;
-}
